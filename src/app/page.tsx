66c713984b8a13d2668d83763ddb0bf11ad19d00
Valeria Gora <v.gora@watched.tech>
2024-01-05 14:53:21 +0200

    image option images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 import { ReactECharts } from "@/components/ReactECharts";
+import { graphic } from "echarts";
 import styles from "./page.module.css";
+// import "echarts/lib/component/grid";
 
-const seriesLabel = {
-  show: true,
-};
-const weatherIcons = {
+const images = {
   Sunny:
     "https://images.unsplash.com/photo-1704187508554-5f38dbbb3495?q=80&w=2784&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   Cloudy:
@@ -13,81 +12,105 @@ const weatherIcons = {
   Showers:
     "https://images.unsplash.com/photo-1704173423733-d01e3466645a?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 };
+const renderWeather = function (param: any, api: any) {
+  const point = api.coord([0, 2]);
+  console.log("p", point);
+
+  return {
+    type: "group",
+    children: [
+      {
+        type: "image",
+        style: {
+          image: api.value(2),
+          x: 2,
+          y: 2,
+          width: 72,
+          height: 72,
+        },
+        position: [point[0], point[1]],
+        // position: [480, 211], // 480, 0 // 69, 140, 211
+      },
+      // {
+      //   type: "text",
+      //   style: {
+      //     text: "text" + "°",
+      //     textFont: api.font({ fontSize: 14 }),
+      //     textAlign: "center",
+      //     textVerticalAlign: "bottom",
+      //   },
+      //   position: [point[0], 80],
+      // },
+    ],
+  };
+};
 const option = {
-  title: {
-    text: "Weather Statistics",
-  },
-  legend: {
-    data: ["City Alpha", "City Beta", "City Gamma"],
-  },
+  xAxis: [
+    {
+      // data: [1, 2, 3, 4],
+      name: "X",
+      type: "value",
+      inverse: true,
+      splitLine: {
+        show: true,
+      },
+      axisTick: {
+        show: true,
+      },
+      axisLabel: {
+        show: true,
+      },
+      axisLine: {
+        show: true,
+      },
+    },
+  ],
   grid: {
-    left: 140,
-  },
-  toolbox: {
-    show: true,
-    feature: {
-      saveAsImage: {},
-    },
-  },
-  xAxis: {
-    type: "value",
-    name: "Days",
-    axisLabel: {
-      formatter: "{value}",
-    },
+    top: 0,
+    bottom: 28,
+    right: "50%",
   },
   yAxis: {
+    name: "Yy",
+    // data: [1, 2, 3, 4],
+    data: ["engaging", "exciting", "intriguing", "close-ended"],
+    position: "right",
     type: "category",
-    inverse: true,
-    data: ["Sunny", "Cloudy", "Showers"],
+    splitLine: {
+      show: true,
+    },
+    axisTick: {
+      // length: 74,
+      show: true,
+    },
     axisLabel: {
-      formatter: function (value: string) {
-        return "{" + value + "| }{value|" + value + "}";
-      },
-      margin: 20,
-      overflow: "break",
-      width: 60,
-      rich: {
-        value: {
-          lineHeight: 30,
-          align: "center",
-        },
-        Sunny: {
-          height: 40,
-          // height: 32,
-          padding: 10,
-          // width: 40,
-          align: "center",
-          backgroundColor: {
-            // width: 40,
-            image: weatherIcons.Sunny,
-          },
-        },
-        Cloudy: {
-          height: 40,
-          padding: 10,
-          align: "center",
-          backgroundColor: {
-            image: weatherIcons.Cloudy,
-          },
-        },
-        Showers: {
-          height: 40,
-          padding: 10,
-          align: "center",
-          backgroundColor: {
-            image: weatherIcons.Showers,
-          },
-        },
-      },
+      margin: 8 + 72 + 8,
+
+      show: true,
+    },
+    axisLine: {
+      show: true,
     },
   },
   series: [
     {
-      name: "City Alpha",
+      type: "custom",
+      renderItem: renderWeather,
+      data: [
+        // [0, 10, images.Cloudy, 40],
+        // [10, 20, images.Showers, 40],
+        [0, 10, images.Sunny],
+      ],
+      // yAxisIndex: 2,
+      z: 11,
+    },
+    {
       type: "bar",
-      data: [165, 170, 30],
-      label: seriesLabel,
+      barWidth: 16,
+      itemStyle: {
+        color: "#25B4C8",
+      },
+      data: [10, 20, 30, 40],
     },
   ],
 };
